c678eeb1ad39909ebe2c475dd9df810aa17b4711
chriszheng <zhengjx_3333@163.com>
2018-11-27 21:44:18 -0500

    update

--- Strategy/sell_stock_by_pct.py
+++ Strategy/sell_stock_by_pct.py
@@ -1,6 +1,7 @@
 import logging
 import robinhood_api.orders as orders
 
+
 def sell_by_pct(stock_list, stock_inventory, pct_threshold_to_sell=0.02):
     """
 
@@ -16,13 +17,13 @@ def sell_by_pct(stock_list, stock_inventory, pct_threshold_to_sell=0.02):
         if float(stock_inventory[stock_symbol]['price']) / float(
                 stock_inventory[stock_symbol]['average_buy_price']) - 1 > pct_threshold_to_sell:
 
-            orders.order_sell_market(symbol=stock_symbol,quantity=stock_inventory[stock_symbol]['quantity'])
+            orders.order_sell_market(symbol=stock_symbol, quantity=stock_inventory[stock_symbol]['quantity'])
             logger.info("Stock {stock_symbol} will be sold {units} at {current_price} with {average_buy_price}"
                         .format(stock_symbol=stock_symbol,
                                 units=stock_inventory[stock_symbol]['quantity'],
                                 current_price=float(stock_inventory[stock_symbol]['price']),
                                 average_buy_price=float(
                                     stock_inventory[stock_symbol]['average_buy_price']))
-                                )
+                        )
         else:
-            logger.info("Stock {stock_symbol} is not good to sell").format(stock_symbol=stock_symbol)
+            logger.info("Stock {stock_symbol} is not good to sell".format(stock_symbol=stock_symbol))
