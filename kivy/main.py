fc8d9b8f6ff53ab7a7da3b80a178a59e8422087c
chriszheng <zhengjx_3333@163.com>
2019-07-14 21:04:33 -0400

    add kivy

--- kivy/main.py
+++ kivy/main.py
@@ -9,14 +9,10 @@ import keras
 class SimpleApp(App):
 
     def build(self):
-
         self.textInput = kivy.uix.textinput.TextInput()
         self.label = kivy.uix.label.Label(text="Your Message.")
         self.button = kivy.uix.button.Button(text="Click Me.")
-
-        message = self.textInput.text + ' with Keras version ' + keras.__version__
-
-        self.button.bind(on_press=self.displayMessage(message))
+        self.button.bind(on_press=self.displayMessage)
         self.boxLayout = kivy.uix.boxlayout.BoxLayout(orientation="vertical")
         self.boxLayout.add_widget(self.textInput)
         self.boxLayout.add_widget(self.label)
@@ -24,8 +20,8 @@ class SimpleApp(App):
 
         return self.boxLayout
 
-    def displayMessage(self, message):
-        self.label.text = message
+    def displayMessage(self, btn):
+        self.label.text = self.textInput.text + keras.__version__
 
 
 if __name__ == "__main__":
